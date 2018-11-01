4126909a1e9c42601cb7880e46d42edfb7fcfc79
FranciscoBianor <bianormedeiros@gmail.com>
2018-11-01 18:26:48 -0300

    Add files via upload

---
+++ www/j1.js
@@ -0,0 +1,24 @@
+
+function Init(){
+var valor=00200020002;
+
+  document.getElementById("batata").innerHTML =valor;
+    
+    document.getElementById("batata").innerHTML =involt.pin.P[13];
+valor=involt.pin.P[13];
+    document.getElementById("batata").innerHTML =valor;
+  
+}
+
+function leitura(){
+  document.getElementById("batata").innerHTML = 4353;
+
+  document.getElementById("batata").innerHTML = involt.pin.P[12];
+
+  document.getElementById("batata").innerHTML = involt.pin.P[11];
+
+  document.getElementById("batata").innerHTML = involt.pin.P[10];
+  
+}
+
+
